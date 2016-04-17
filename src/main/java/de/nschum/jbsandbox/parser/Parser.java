cda834477775d0cd3743af2a0e31d45e2435bb30
Nikolaj Schumacher <git@nschum.de>
2016-04-17 22:40:07 +0200

    Implement parser

---
+++ src/main/java/de/nschum/jbsandbox/parser/Parser.java
@@ -0,0 +1,96 @@
+package de.nschum.jbsandbox.parser;
+
+import de.nschum.jbsandbox.SourceLocation;
+import de.nschum.jbsandbox.grammar.Grammar;
+import de.nschum.jbsandbox.grammar.GrammarRule;
+import de.nschum.jbsandbox.grammar.GrammarToken;
+import de.nschum.jbsandbox.scanner.ScannerToken;
+
+import java.util.*;
+
+import static de.nschum.jbsandbox.grammar.Grammar.EOF;
+import static de.nschum.jbsandbox.grammar.Grammar.EPSILON;
+
+/**
+ * Parses a Grammar
+ * <p>
+ * LL(1) parser for grammar, creates a ParseTree from a list of tokens.
+ */
+public class Parser {
+
+    private Grammar grammar;
+    private ParserTable parserTable;
+
+    public Parser(Grammar grammar) {
+        this.grammar = grammar;
+        ParserFirstSets parserFirstSets = new ParserFirstSets(grammar);
+        parserTable = new ParserTable(grammar, parserFirstSets, new ParserFollowSets(grammar, parserFirstSets));
+    }
+
+    public ParserTree parse(List<ScannerToken> tokens) throws UnexpectedTokenException, MissingTokenException {
+
+        // stack of started rules / tree output
+        Stack<ParserTree> stack = new Stack<>();
+        final ParserTree rootTree = new ParserTree(grammar.getStartSymbol());
+        stack.push(rootTree);
+
+        Iterator<ScannerToken> unreadTokens = iterateWithEOF(tokens);
+        ScannerToken nextToken = unreadTokens.next();
+
+        while (stack.size() > 0 && !stack.peek().getToken().equals(EOF)) {
+            if (nextToken.getGrammarToken().equals(stack.peek().getToken())) {
+                // process terminal and remove it from input
+                ParserTree output = stack.pop();
+                output.setContent(Optional.of(nextToken.getContent()));
+                nextToken = unreadTokens.next();
+            } else {
+                // apply rule and rewrite stack
+                ParserTree tree = stack.pop();
+
+                GrammarRule rule = lookUpRule(nextToken, tree.getToken());
+
+                for (GrammarToken token : reverse(rule.getRightHandSide())) {
+                    tree.setRule(rule);
+                    if (!token.equals(EPSILON)) {
+                        ParserTree newTree = new ParserTree(token);
+                        tree.addChild(newTree);
+                        stack.push(newTree);
+                    }
+                }
+            }
+        }
+
+        if (unreadTokens.hasNext()) {
+            throw new UnexpectedTokenException(nextToken);
+        }
+
+        return rootTree;
+    }
+
+    private GrammarRule lookUpRule(ScannerToken nextToken, GrammarToken state) throws MissingTokenException, UnexpectedTokenException {
+        Optional<GrammarRule> optionalRule = parserTable.getRuleForTerminal(nextToken.getGrammarToken(), state);
+        if (!optionalRule.isPresent()) {
+            if (nextToken.getGrammarToken().equals(EOF)) {
+                throw new MissingTokenException(state);
+            } else {
+                throw new UnexpectedTokenException(nextToken);
+            }
+        }
+        return optionalRule.get();
+    }
+
+    /**
+     * Iterate over tokens with EOF appended.
+     */
+    private Iterator<ScannerToken> iterateWithEOF(List<ScannerToken> tokens) {
+        List<ScannerToken> tokensWithEOF = new ArrayList<>(tokens);
+        tokensWithEOF.add(new ScannerToken(EOF, "", new SourceLocation(0, 0)));
+        return tokensWithEOF.iterator();
+    }
+
+    private <T> List<T> reverse(List<T> list) {
+        ArrayList<T> reverseList = new ArrayList<>(list);
+        Collections.reverse(reverseList);
+        return reverseList;
+    }
+}
---
+++ src/test/java/de/nschum/jbsandbox/parser/ParserTests.java
@@ -0,0 +1,216 @@
+package de.nschum.jbsandbox.parser;
+
+import de.nschum.jbsandbox.SourceLocation;
+import de.nschum.jbsandbox.grammar.Grammar;
+import de.nschum.jbsandbox.grammar.GrammarRule;
+import de.nschum.jbsandbox.grammar.GrammarToken;
+import de.nschum.jbsandbox.scanner.ScannerToken;
+import org.hamcrest.Matcher;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import static de.nschum.jbsandbox.grammar.Grammar.EPSILON;
+import static de.nschum.jbsandbox.parser.MockTokens.*;
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+public class ParserTests {
+
+    // matchers
+
+    private Matcher<ParserTree> parserTreeWithRule(GrammarRule rule) {
+        return hasProperty("rule", equalTo(rule));
+    }
+
+    private Matcher<ParserTree> parserTreeWithToken(GrammarToken token) {
+        if (token.isTerminal()) {
+            return allOf(
+                    hasProperty("token", equalTo(token)),
+                    hasProperty("content", equalTo(Optional.of(token.toString()))));
+        } else {
+            return hasProperty("token", equalTo(token));
+        }
+    }
+
+    @SafeVarargs
+    final private Matcher<ParserTree> parserTreeWithChildren(Matcher<ParserTree>... itemMatchers) {
+        return hasProperty("children", contains(itemMatchers));
+    }
+
+    private Matcher<ParserTree> parserTreeWithChildren(Matcher<Collection<?>> itemsMatcher) {
+        return hasProperty("children", itemsMatcher);
+    }
+
+    // mocks
+
+    private Grammar mockGrammarWithRules(GrammarRule... rules) {
+        return new Grammar() {
+            @Override
+            public List<GrammarRule> getRules() {
+                return Arrays.asList(rules);
+            }
+
+            @Override
+            public GrammarToken getStartSymbol() {
+                return S;
+            }
+        };
+    }
+
+    private List<ScannerToken> mockScannerTokens(GrammarToken... grammarTokens) {
+        return IntStream.range(0, grammarTokens.length)
+                .mapToObj(i -> {
+                    final GrammarToken token = grammarTokens[i];
+                    return new ScannerToken(token, token.toString(), new SourceLocation(0, i));
+                })
+                .collect(toList());
+    }
+
+    // tests
+
+    @Test
+    public void shouldParseRuleOfLiterals() throws Exception {
+        // given
+        GrammarRule rule = new GrammarRule(S, a, b);
+        Grammar grammar = mockGrammarWithRules(rule);
+
+        // when
+        Parser parser = new Parser(grammar);
+
+        // then
+        final ParserTree parserTree = parser.parse(mockScannerTokens(a, b));
+        assertThat(parserTree, parserTreeWithRule(rule));
+        assertThat(parserTree, parserTreeWithChildren(
+                parserTreeWithToken(a),
+                parserTreeWithToken(b)
+        ));
+    }
+
+    @Test
+    public void shouldParseIndirectRuleOfLiterals() throws Exception {
+        // given
+        GrammarRule rule1 = new GrammarRule(S, a, B, c);
+        GrammarRule rule2 = new GrammarRule(B, b);
+        Grammar grammar = mockGrammarWithRules(rule1, rule2);
+
+        // when
+        Parser parser = new Parser(grammar);
+
+        // then
+        final ParserTree parserTree = parser.parse(mockScannerTokens(a, b, c));
+        assertThat(parserTree, parserTreeWithRule(rule1));
+        assertThat(parserTree, parserTreeWithChildren(
+                parserTreeWithToken(a),
+                allOf(
+                        parserTreeWithToken(B),
+                        parserTreeWithRule(rule2),
+                        parserTreeWithChildren(parserTreeWithToken(b))),
+                parserTreeWithToken(c)
+        ));
+    }
+
+    @Test
+    public void shouldParseRuleWithEpsilon() throws Exception {
+        // given
+        GrammarRule rule1 = new GrammarRule(S, a, B, c);
+        GrammarRule rule2 = new GrammarRule(B, EPSILON);
+        Grammar grammar = mockGrammarWithRules(rule1, rule2);
+
+        // when
+        Parser parser = new Parser(grammar);
+
+        // then
+        final ParserTree parserTree = parser.parse(mockScannerTokens(a, c));
+        assertThat(parserTree, parserTreeWithRule(rule1));
+        assertThat(parserTree, parserTreeWithChildren(
+                parserTreeWithToken(a),
+                allOf(
+                        parserTreeWithToken(B),
+                        parserTreeWithRule(rule2),
+                        parserTreeWithChildren(empty())),
+                parserTreeWithToken(c)
+        ));
+    }
+
+    @Test(expected = UnexpectedTokenException.class)
+    public void shouldThrowsExceptionForUnexpectedToken() throws Exception {
+        // given
+        GrammarRule rule = new GrammarRule(S, a);
+        Grammar grammar = mockGrammarWithRules(rule);
+
+        // when
+        Parser parser = new Parser(grammar);
+
+        // then
+        try {
+            parser.parse(mockScannerTokens(b));
+        } catch (UnexpectedTokenException e) {
+            assertThat(e.getMessage(), equalTo("No rule for parsing b"));
+            assertThat(e.getLocation(), equalTo(new SourceLocation(0, 0)));
+            throw e;
+        }
+    }
+
+    @Test(expected = UnexpectedTokenException.class)
+    public void shouldThrowsExceptionForTrailingTokens() throws Exception {
+        // given
+        GrammarRule rule = new GrammarRule(S, a);
+        Grammar grammar = mockGrammarWithRules(rule);
+
+        // when
+        Parser parser = new Parser(grammar);
+
+        // then
+        try {
+            parser.parse(mockScannerTokens(a, b));
+        } catch (UnexpectedTokenException e) {
+            assertThat(e.getMessage(), equalTo("No rule for parsing b"));
+            assertThat(e.getLocation(), equalTo(new SourceLocation(0, 1)));
+            throw e;
+        }
+    }
+
+    @Test(expected = MissingTokenException.class)
+    public void shouldThrowsExceptionForMissingTerminal() throws Exception {
+        // given
+        GrammarRule rule = new GrammarRule(S, a, b);
+        Grammar grammar = mockGrammarWithRules(rule);
+
+        // when
+        Parser parser = new Parser(grammar);
+
+        // then
+        try {
+            parser.parse(mockScannerTokens(a));
+        } catch (MissingTokenException e) {
+            assertThat(e.getMessage(), equalTo("Expected token b"));
+            throw e;
+        }
+    }
+
+    @Test(expected = MissingTokenException.class)
+    public void shouldThrowsExceptionForMissingSymbol() throws Exception {
+        // given
+        GrammarRule rule1 = new GrammarRule(S, a, B);
+        GrammarRule rule2 = new GrammarRule(B, c);
+        GrammarRule rule3 = new GrammarRule(B, b);
+        Grammar grammar = mockGrammarWithRules(rule1, rule2, rule3);
+
+        // when
+        Parser parser = new Parser(grammar);
+
+        // then
+        try {
+            parser.parse(mockScannerTokens(a));
+        } catch (MissingTokenException e) {
+            assertThat(e.getMessage(), equalTo("Expected token"));
+            throw e;
+        }
+    }
+}
